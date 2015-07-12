00ec6cc59cf29ae8484cac9acce068cad7af2748
Michael Lee <michael_lee.org@hotmail.com>
2015-07-12 16:54:11 -0400

    Initial Commit

---
+++ PicPoll/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  PicPoll
+//
+//  Created by Michael Lee on 7/12/15.
+//  Copyright (c) 2015 CodeCrispy. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+	override func viewDidLoad() {
+		super.viewDidLoad()
+		// Do any additional setup after loading the view, typically from a nib.
+	}
+
+	override func didReceiveMemoryWarning() {
+		super.didReceiveMemoryWarning()
+		// Dispose of any resources that can be recreated.
+	}
+
+
+}
+
